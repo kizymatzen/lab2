e740c7a211e0af083c6bdd06add9f8021d699017
Kizy <64035295+kizymatzen@users.noreply.github.com>
2022-02-16 20:00:00 -0700

    Update Block.cs

--- lab2/Block.cs
+++ lab2/Block.cs
@@ -1,19 +1,54 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
 using System.Transactions;
+using Lab2_Kizy;
 
-namespace RootCoin
+namespace Lab2_Kizy
 {
-    internal class Block
+    class Block
     {
-        private int v1;
-        private string v2;
-        private List<Transaction> transactions;
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string timestamp { get; set; }
+        public string Hash { get; set; }
+        public string Timestamp { get; private set; }
+        public int Nonce { get; private set; }
+        public List<Transaction> Transactions { get; private set; }
 
-        public Block(int v1, string v2, List<Transaction> transactions)
+
+        public Block(int index, string timestap, List<Transaction> transactions, string previousHash = "")
         {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.transactions = transactions;
+            this.Index = index;
+            this.timestamp = timestamp;
+            this.Transactions = transactions;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.Timestamp + this.Transactions.ToString();
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0, difficulty) != new String('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                //Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("Block has been mined: " + this.Hash); 
         }
     }
 }
