3016331d1c1897ce12787ddf47496b3e4791e250
Kizy Matzenbacher <kizymatzen@gmail.com>
2023-02-16 23:54:59 -0700

    Add project files.

---
+++ lab2/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Transactions;
+
+namespace RootCoin
+{
+    internal class Block
+    {
+        private int v1;
+        private string v2;
+        private List<Transaction> transactions;
+
+        public Block(int v1, string v2, List<Transaction> transactions)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.transactions = transactions;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+using System.Transactions;
+
+namespace RootCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key1.publicKey();
+
+            Blockchain rootcoin = new.Blockchain(2, 100);
+
+            Console.WriteLine("Start the Miner.");
+
+            string blockJSON = JsonConvert.SerializeObject(rootcoin, formatting.Indented);
+            Console.WriteLine(BlockJSON);
+
+            if (rootcoin.IsChainValid)
+
+
+            Block newBlock = new Block(1, DateTime.Now.ToString("yyyyMMddHmmssffff"), "amount: 50", "");
+            string blockJSON = JsonConvert.SerializeObject(newBlock, Formatting.Indented);
+            Console.WriteLine(BlockJSON);
+        }
+    }
+    
+    class Blockchain
+    {
+
+    }
+
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string timestamp { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+        
+        public Block(int index, string timestap, string data, string previousHash="")
+        {
+            this.Index = index;
+            this.timestamp = timestamp;
+            this.Data = data;
+            this.PreviousHash = previousHash;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.Index.PreviousHash + this.Timestamp + this.Data;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
+       
+       
+}
